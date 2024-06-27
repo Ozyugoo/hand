8e1259fbbd9db19eceab44808a437d840eef7f48
Ozyugoo <stellaozy22@gmail.com>
2024-06-27 05:07:04 +0100

    Feat: Initilalize repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hand Project
+
+This project is a landing page for a hand web application.
+
+The page is built with React and Tailwind.
+
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+        plus: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      colors: {
+        light: "#F5F4F6",
+        customYellow: "#FDCA09",
+        accent: "#FEBC0AB2",
+        gray: "#5A6675",
+        darkBtn: "#0B0B0B",
+        darkMeet: "#101010",
+        brownish: "#FEF1E0",
+        lightFoot: "#F5F5F5",
+        copyRight: "#525D6A",
+        iconBg: "#1E1E1E",
+        figures: "#FEC00A",
+        focus: "#C82090",
+      },
+    },
+    plugins: [],
+  },
+};
