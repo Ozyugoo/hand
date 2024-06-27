db5a0c132e999555030e9de53af22db3bf9e671e
Ozyugoo <stellaozy22@gmail.com>
2024-06-27 19:24:26 +0100

    File: Footer done

--- src/Layout/Page.jsx
+++ src/Layout/Page.jsx
@@ -1,11 +1,13 @@
 import Header from "../Layout/header/Header.jsx";
 import Main from "../Layout/main/Main.jsx";
+import Footer from "./footer/Footer.jsx";
 
 function Page() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer/>
     </div>
   );
 }
